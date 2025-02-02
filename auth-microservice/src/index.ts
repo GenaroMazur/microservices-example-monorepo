b06e90201eea53a8984833056fd23ce1eb848077
GenaroMazur <genaromazur.business@gmail.com>
2025-02-02 16:14:43 -0300

    Agregar middleware de seguridad y logging en el microservicio de autenticación

--- auth-microservice/src/index.ts
+++ auth-microservice/src/index.ts
@@ -12,12 +12,29 @@ import { Environments } from "./enums/Environments.enum";
 import Core from "./infrastructure/Core";
 import ExpressServer from "./infrastructure/express.server";
 import { getEnvironments } from "./utils/getEnvironments.utils";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import { helmetOptions } from "./utils/helmet.options";
+import { logger } from "./utils/logger";
 
 const PORT = Number(getEnvironments(Environments.AUTH_MICROSERVICE_PORT));
 if (Number.isNaN(PORT) || PORT < 1 || PORT > 65535)
   throw new Error("Invalid port");
 
 const expressServer = new ExpressServer(PORT);
+
+const NODE_ENV = getEnvironments(Environments.NODE_ENV);
+expressServer.app.use(
+  cors({ origin: "*" }),
+  express.urlencoded({ extended: true }),
+  express.json(),
+  helmet(helmetOptions),
+  morgan(NODE_ENV === "production" ? "common" : "dev", {
+    stream: { write: (message) => logger.http(message.replace("\n", "")) },
+  }),
+);
 
 const core = Core.getInstance();
 
